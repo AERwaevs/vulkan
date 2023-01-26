205ca5ae08fd380f7199eb46e6cb241256edc8a4
Hammy Cooper <thehamstarr@gmail.com>
2023-01-26 23:00:08 +0800

    remove Surfaces & Devices from VulkanRenderer

--- VulkanRenderer.cpp
+++ VulkanRenderer.cpp
@@ -2,29 +2,24 @@
 
 #include <vk/Extensions.h>
 #include <vk/Layers.h>
+#include <vk/Instance.h>
 
 namespace AEON::Graphics
 {
-    AEON_API VulkanRenderer::VulkanRenderer()
-    :   m_instance( vk::Instance::create
-        (
-            vk::GetRequiredExtensions(),
-            vk::GetRequiredLayers ()
-        ) )
-    {
-        //m_surfaces.push_back
-        //(
-        //    new vk::Surface( m_instance, )
-        //);
-        //m_devices.push_back
-        //(
-        //    m_instance->GetPhysicalDevices().front()
+    
+AEON_API VulkanRenderer::VulkanRenderer()
+:   m_instance( vk::Instance::create
+    (
+        vk::GetRequiredExtensions(),
+        vk::GetRequiredLayers ()
+    ) )
+{
 
-        //);
-    }
+}
 
-    AEON_API VulkanRenderer::~VulkanRenderer()
-    {
-        gladLoaderUnloadVulkan();
-    }
+AEON_API VulkanRenderer::~VulkanRenderer()
+{
+    gladLoaderUnloadVulkan();
+}
+
 }
--- VulkanRenderer.h
+++ VulkanRenderer.h
@@ -11,12 +11,10 @@ namespace AEON::Graphics
     class AEON_DLL VulkanRenderer : public Inherit< Renderer, VulkanRenderer >
     {
     public:
-        VulkanRenderer();
+                        VulkanRenderer();
     protected:
-        virtual ~VulkanRenderer();
+        virtual         ~VulkanRenderer();
     private:
         Shared<vk::Instance>        m_instance;
-        List<Shared<vk::Device>>    m_devices;
-        List<Shared<vk::Surface>>   m_surfaces;
     };
 }
