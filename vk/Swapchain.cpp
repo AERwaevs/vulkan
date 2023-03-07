9ad5a1d71a229f9259592e6de0924429e45da7f9
Hammy Cooper <thehamstarr@gmail.com>
2023-03-07 22:33:13 +0800

    add SelectSwap**() functions

--- vk/Swapchain.cpp
+++ vk/Swapchain.cpp
@@ -1,6 +1,5 @@
 #include "Swapchain.h"
 
-
 namespace AEON::Graphics::vk
 {
 
@@ -29,6 +28,54 @@ SwapChainSupportDetails QuerySwapchainSupport( VkPhysicalDevice device, VkSurfac
     }
 
     return details;
+}
+
+VkSurfaceFormatKHR SelectSwapSurfaceFormat( const SwapChainSupportDetails& details, VkSurfaceFormatKHR preferred )
+{
+    const auto  default_format      = VK_FORMAT_B8G8R8A8_UNORM;
+    const auto  default_colorspace  = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR;
+
+    if( details.formats.empty() || ( details.formats.size() == 1 && details.formats[0].format == VK_FORMAT_UNDEFINED ) )
+    {
+        AE_WARN( "Swapchain format undefined, using fallback" ); return { default_format, default_colorspace };
+    }
+
+    for( const auto& available : details.formats )
+    {
+        if( available.format == preferred.format && available.colorSpace == preferred.colorSpace ) return available;
+    }
+
+    AE_WARN( "Preferred swapchain format unavailable, trying default" );
+    for( const auto& available : details.formats )
+    {
+        if( available.format == default_format && available.colorSpace == default_colorspace ) return available;
+    }
+
+    AE_WARN( "Default swapchain format unavailable, using first available" );
+    return details.formats[0];
+}
+
+VkPresentModeKHR SelectSwapPresentMode( const SwapChainSupportDetails& details, VkPresentModeKHR preferred )
+{
+    const auto default_present_mode = VK_PRESENT_MODE_MAILBOX_KHR; 
+
+    for( auto available : details.present_modes ) { if( available == preferred ) return available; }
+    AE_WARN( "Preferred present mode unavailable, trying default" );
+    for( auto available : details.present_modes ) { if( available == default_present_mode ) return available; }
+    AE_WARN( "Preferred present mode unavailable, using fallback" );
+    return VK_PRESENT_MODE_FIFO_KHR;
+}
+
+VkExtent2D SelectSwapExtent( const SwapChainSupportDetails& details, uint32_t width, uint32_t height )
+{
+    const VkSurfaceCapabilitiesKHR& capabilities = details.capabilities;
+
+    if( capabilities.currentExtent.width != std::numeric_limits<uint32_t>::max() ) return capabilities.currentExtent;
+    else return VkExtent2D
+    {
+        std::max( capabilities.minImageExtent.width, std::min( capabilities.maxImageExtent.width, width ) ),
+        std::max( capabilities.minImageExtent.height, std::min( capabilities.maxImageExtent.height, height ) )
+    };
 }
 
 }// namespace AEON::Graphics::vk
--- vk/Swapchain.h
+++ vk/Swapchain.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <Base/Base.h>
 #include <glad/vulkan.h>
+
+#include <Core/Log.h>
 
 namespace AEON::Graphics::vk
 {
@@ -13,5 +16,11 @@ struct SwapChainSupportDetails
 };
 
 SwapChainSupportDetails QuerySwapchainSupport( VkPhysicalDevice device, VkSurfaceKHR surface );
+VkSurfaceFormatKHR      SelectSwapSurfaceFormat( const  SwapChainSupportDetails& details, 
+                                                        VkSurfaceFormatKHR preferred );
+VkPresentModeKHR        SelectSwapPresentMode(   const  SwapChainSupportDetails& details, 
+                                                        VkPresentModeKHR preferred );
+VkExtent2D              SelectSwapExtent(        const  SwapChainSupportDetails& details,
+                                                        uint32_t width, uint32_t height );
 
 }// namespace AEON::Graphics::vk
