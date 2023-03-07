9a7d4df9c192b9844f2acf77cf9f40a408b3b33e
Hammy Cooper <thehamstarr@gmail.com>
2023-03-07 17:47:48 +0800

    add QuerySwapChainSupport

--- vk/Swapchain.cpp
+++ vk/Swapchain.cpp
@@ -4,4 +4,31 @@
 namespace AEON::Graphics::vk
 {
 
+SwapChainSupportDetails QuerySwapchainSupport( VkPhysicalDevice device, VkSurfaceKHR surface )
+{
+    SwapChainSupportDetails details;
+
+    vkGetPhysicalDeviceSurfaceCapabilitiesKHR( device, surface, &details.capabilities );
+
+    uint32_t format_count;
+    vkGetPhysicalDeviceSurfaceFormatsKHR( device, surface, &format_count, VK_NULL_HANDLE );
+
+    if( format_count > 0 )
+    {
+        details.formats.resize( format_count );
+        vkGetPhysicalDeviceSurfaceFormatsKHR( device, surface, &format_count, details.formats.data() );
+    }
+
+    uint32_t present_mode_count;
+    vkGetPhysicalDeviceSurfacePresentModesKHR( device, surface, &present_mode_count, VK_NULL_HANDLE );
+
+    if( present_mode_count > 0 )
+    {
+        details.present_modes.resize( present_mode_count );
+        vkGetPhysicalDeviceSurfacePresentModesKHR( device, surface, &present_mode_count, details.present_modes.data() );
+    }
+
+    return details;
+}
+
 }// namespace AEON::Graphics::vk
--- vk/Swapchain.h
+++ vk/Swapchain.h
@@ -1,7 +1,17 @@
 #pragma once
 
+#include <glad/vulkan.h>
+
 namespace AEON::Graphics::vk
 {
 
+struct SwapChainSupportDetails
+{
+    VkSurfaceCapabilitiesKHR        capabilities;
+    std::vector<VkSurfaceFormatKHR> formats;
+    std::vector<VkPresentModeKHR>   present_modes;
+};
+
+SwapChainSupportDetails QuerySwapchainSupport( VkPhysicalDevice device, VkSurfaceKHR surface );
 
 }// namespace AEON::Graphics::vk
