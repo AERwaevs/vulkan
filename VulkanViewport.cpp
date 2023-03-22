86a57f486f975ee1ba850aaff12c83e4e5130847
Hammy Cooper <thehamstarr@gmail.com>
2023-03-22 15:33:42 +0800

    fix surface creation args

--- VulkanViewport.cpp
+++ VulkanViewport.cpp
@@ -9,7 +9,7 @@ namespace AEON::Graphics
 VulkanViewport::VulkanViewport( Window* window )
 : Viewport{ window },
   m_instance{ vk::Instance::instance() },
-  m_surface{ vk::Surface::create( m_instance.get(), window ) },
+  m_surface{ vk::Surface::create( m_instance, window ) },
   m_physical_device{ m_instance->physical_devices().front() },  //TODO remove hardcoded physical device selection
   m_device{ vk::Device::create( m_physical_device, m_surface ) },
   m_swapchain_prefs{}
