57e531ec1876e557498f65dd131f9a806b776f16
Hammy Cooper <thehamstarr@gmail.com>
2023-06-15 14:37:11 +0800

    add Pipeline, PipelineState

---
+++ inc/vk/state/Pipeline.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <glad/vulkan.h>
+
+#include <Base/Base.h>
+
+namespace aer::gfx::vk
+{
+
+}   // namespace aer::gfx::vk
---
+++ inc/vk/state/PipelineState.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <glad/vulkan.h>
+
+#include <Base/Base.h>
+
+namespace aer::gfx::vk
+{
+    
+
+    
+} // namespace aer::gfx::vk
