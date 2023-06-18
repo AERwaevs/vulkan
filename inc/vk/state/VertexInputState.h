f45946f45f9666fe3b92dfefce65be9bdefe7304
ÆRwævs <thehamstarr@gmail.com>
2023-06-18 22:57:15 +0800

    add VertexInputState

---
+++ inc/vk/state/VertexInputState.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <Base/Base.h>
+
+namespace aer::gfx::vk
+{
+    
+class VertexInputState : public Object, public Interfaces< VertexInputState, ICreate, ITypeInfo >
+{
+    
+}
+    
+} // namespace aer::gfx::vk
